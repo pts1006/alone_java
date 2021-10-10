c921dca15e53496be3c51169dc247992bf6af083
pts1006 <76603521+pts1006@users.noreply.github.com>
2021-10-10 22:06:56 +0900

    Add files via upload

---
+++ javaPractice06/src/co/jjw/prc/PrinterExample.java
@@ -0,0 +1,18 @@
+package co.jjw.prc;
+
+public class PrinterExample {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		// Printer printer = new Printer();	// Printer No 'static
+		
+		Printer.println(10);
+		Printer.println(true);
+		Printer.println(5.7);
+		Printer.println("홍길동");
+		// Printer class's name
+		
+	}
+
+}
