8606ca1b4acdc77e91b73ac9b1ab5d07a04dfb46
pts1006 <76603521+pts1006@users.noreply.github.com>
2021-11-17 23:22:16 +0900

    Add files via upload

---
+++ java07/src/co/jjw/prc/C.java
@@ -0,0 +1,10 @@
+package co.jjw.prc;
+
+public class C {
+
+    /* public void method() {
+        A a = new A();
+        a.field = "value";
+        a.method();
+    } */
+}
---
+++ java07/src/co/jjw/prc/D.java
@@ -0,0 +1,12 @@
+package co.jjw.prc;
+
+import co.jjw.pra.A;
+
+public class D extends A {
+
+    public D() {
+        super();
+        this.field = "value";
+        this.method();
+    }
+}
