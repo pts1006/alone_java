b278629bb7f28ac2a8ba30b47ab652020cddbbec
pts1006 <76603521+pts1006@users.noreply.github.com>
2021-12-03 19:35:47 +0900

    Add files via upload

---
+++ java11/co/pts/prc/Member.java
@@ -0,0 +1,29 @@
+public class Member {
+/* 객체 동등 비교 메소드 */
+    public String id;
+
+    public Member(String id) {
+
+        this.id = id;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+
+        if (obj instanceof Member) {    // 매개값이 Member타입인지 확인
+            Member member = (Member) obj;
+            if (id.equals(member.id)) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    /* 메소드 재정의 추가 */
+    @Override
+    public int hashCode() {
+
+        return id.hashCode();
+    }
+}
