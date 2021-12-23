056a66bd3b189141f16854b5733a167cb8fee2a5
pts1006 <76603521+pts1006@users.noreply.github.com>
2021-12-23 16:13:13 +0900

    Add files via upload

---
+++ java11/src/co/pts/prc/StringTrimExample.java
@@ -0,0 +1,14 @@
+package co.pts.prc;
+
+public class StringTrimExample {
+
+	public static void main(String[] args) {
+		/* 앞뒤 공백 제거 */
+		String tel1 = "  02";
+		String tel2 = "123  ";
+		String tel3 = "  1234  ";
+		
+		String tel = tel1.trim() + tel2.trim() + tel3.trim();
+		System.out.println(tel);
+	}
+}
