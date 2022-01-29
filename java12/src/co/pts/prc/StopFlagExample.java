97705d23c4c1db4946acb85ac3b4ffb976ff7ba1
pts1006 <76603521+pts1006@users.noreply.github.com>
2022-01-29 15:39:49 +0900

    Add files via upload

---
+++ java12/src/co/pts/prc/StopFlagExample.java
@@ -0,0 +1,18 @@
+package co.pts.prc;
+
+public class StopFlagExample {
+
+	public static void main(String[] args) {
+		
+		PrintThread1 printThread = new PrintThread1();
+		printThread.start();
+		
+		try {
+			Thread.sleep(1000);
+		} catch (InterruptedException e) {
+			// TODO: handle exception
+		}
+		
+		printThread.setStop(true);
+	}
+}
