34296e8e8a096c6326684d2cfded9609ae5789a8
pts1006 <76603521+pts1006@users.noreply.github.com>
2022-02-01 20:50:05 +0900

    Add files via upload

---
+++ java12/src/co/pts/prc/ThreadInfoExample.java
@@ -0,0 +1,26 @@
+package co.pts.prc;
+
+import java.util.Map;
+import java.util.Set;
+
+public class ThreadInfoExample {
+
+	public static void main(String[] args) {
+		// 현재 실행 중인 스레드 정보
+
+		AutoSaveThread autoSaveThread = new AutoSaveThread();
+		autoSaveThread.setName("AutoSaveThread");
+		autoSaveThread.setDaemon(true);
+		autoSaveThread.start();
+		
+		Map<Thread, StackTraceElement[]> map = Thread.getAllStackTraces();
+		Set<Thread> threads = map.keySet();
+		
+		for (Thread thread : threads) {
+			System.out.println("Name: " + thread.getName() + ((thread.isDaemon())?"(데몬)": "(주)"));
+			System.out.println("\t" + "소속그룹: " + thread.getThreadGroup().getName());
+			System.out.println();
+		}
+	}
+
+}
