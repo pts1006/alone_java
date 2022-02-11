a674fd3e70f45fac3811f26aac5ec9885540f94c
pts1006 <76603521+pts1006@users.noreply.github.com>
2022-02-11 10:08:48 +0900

    Add files via upload

---
+++ java13/src/co/pts/prb/BoundedTypeParameterExample.java
@@ -0,0 +1,15 @@
+package co.pts.prb;
+
+public class BoundedTypeParameterExample {
+
+	public static void main(String[] args) {
+		
+		// String str = Util.compare("a", "b");		String은 Number타입이 아님
+		
+		int result1 = Util.compare(10, 20);
+		System.out.println(result1);
+		
+		int result2 = Util.compare(4.5, 3);
+		System.out.println(result2);
+	}
+}
