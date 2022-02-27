52322df4f9a9c21959c3a55ada4f779463fec7d4
pts1006 <76603521+pts1006@users.noreply.github.com>
2022-02-27 19:37:50 +0900

    Add files via upload

---
+++ java14/src/co/pts/prc/PredicateAndOrNegateExample.java
@@ -0,0 +1,36 @@
+package co.pts.prc;
+
+import java.util.function.IntPredicate;
+
+public class PredicateAndOrNegateExample {
+
+	public static void main(String[] args) {
+		// Predicate 간의 논리 연산
+		
+		// 2의 배수 검사
+		IntPredicate predicateA = a -> a % 2 == 0;
+
+		// 3의 배수 검사
+		IntPredicate predicateB = a -> a % 3 == 0;
+		
+		IntPredicate predicateAB;
+		boolean result;
+		
+		// and()
+		predicateAB = predicateA.and(predicateB);
+		result = predicateAB.test(9);
+		System.out.println("9는 2와 3의 배수입니까? " + result);
+		
+		// or()
+		predicateAB = predicateA.or(predicateB);
+		result = predicateAB.test(9);
+		System.out.println("9는 2와 3의 배수이빈까? " + result);
+		
+		// negate()
+		predicateAB = predicateA.negate();
+		result = predicateAB.test(9);
+		System.out.println("9는 홀수입니까? " + result);
+		
+	}
+
+}
