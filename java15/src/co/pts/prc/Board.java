d99ebbda312f2af55cba25263da0a84805e8a0ab
pts1006 <76603521+pts1006@users.noreply.github.com>
2022-03-07 23:56:44 +0900

    Add files via upload

---
+++ java15/src/co/pts/prc/Board.java
@@ -0,0 +1,16 @@
+package co.pts.prc;
+
+public class Board {
+	// 게시물 정보 객체
+	
+	String subject;
+	String content;
+	String writer;
+	
+	public Board(String subject, String content, String writer) {
+		
+		this.subject = subject;
+		this.content = content;
+		this.writer = writer;
+	}
+}
