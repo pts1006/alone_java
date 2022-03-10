184112fa805cf292651a7c08dd914883a5969b27
pts1006 <76603521+pts1006@users.noreply.github.com>
2022-03-10 21:38:08 +0900

    Add files via upload

---
+++ java15/src/co/pts/prc/Member.java
@@ -0,0 +1,31 @@
+package co.pts.prc;
+
+public class Member {
+
+	public String name;
+	public int age;
+	
+	public Member(String name, int age) {
+		
+		this.name = name;
+		this.age = age;
+	}
+	
+	@Override
+	public boolean equals(Object obj) {	// name과 age 값이 같으면 true 리턴
+		
+		if (obj instanceof Member) {
+			Member member = (Member) obj;
+			return member.name.equals(name) && (member.age == age);
+		} else {
+			return false;
+		}
+	}
+	
+	@Override
+	public int hashCode() {	// name과 age값이 같으면 동일한 hashCode가 리턴
+		
+		return name.hashCode() + age;
+				// String의 hashCode() 이용
+	}
+}
