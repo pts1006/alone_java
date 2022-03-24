26d9eb5addceaa8b74d09ad51c992f0c1f351b67
pts1006 <76603521+pts1006@users.noreply.github.com>
2022-03-24 14:03:19 +0900

    Add files via upload

---
+++ java15/src/co/pts/prc/QueueExample.java
@@ -0,0 +1,32 @@
+package co.pts.prc;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class QueueExample {
+
+	public static void main(String[] args) {
+		// Queue를 이용한 메시지 큐
+		
+		Queue<Message> messageQueue = new LinkedList<Message>();
+		
+		messageQueue.offer(new Message("sendMail", "홍길동"));
+		messageQueue.offer(new Message("sendSMS", "신용권"));
+		messageQueue.offer(new Message("sendKakaotalk", "홍두께"));
+		
+		while (!messageQueue.isEmpty()) {
+			Message message = messageQueue.poll();	// 메시지 큐에서 한 개의 메시지 꺼냄			
+			switch (message.command) {
+				case "sendMail":
+					System.out.println(message.to + "님에게 메일을 보냅니다");
+					break;
+				case "sendSMS":
+					System.out.println(message.to + "님에게 SMS를 보냅니다");
+					break;
+				case "sendKakaotalk":
+					System.out.println(message.to + "님에게 카카오톡을 보냅니다");
+					break;
+			}
+		}
+	}
+}
