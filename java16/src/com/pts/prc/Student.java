be921d55ed3e218ac5ffc66b625b74c58bbb44f1
pts1006 <76603521+pts1006@users.noreply.github.com>
2022-04-14 20:54:49 +0900

    Add files via upload

--- java16/src/com/pts/prc/Student.java
+++ java16/src/com/pts/prc/Student.java
@@ -1,6 +1,6 @@
 package com.pts.prc;
 
-public class Student {
+public class Student implements Comparable<Student> {
 
 	private String name;
 	private int score;
@@ -13,4 +13,13 @@ public class Student {
 	
 	public String getName() { return name; }
 	public int getScore() { return score; }
+	
+	@Override
+	public int compareTo(Student o) {
+		
+		return Integer.compare(score, o.score);
+//		score < o.score: 음수 리턴
+//		score == o.score: 0리턴
+//		score > o.score: 양수 리턴
+	}
 }
