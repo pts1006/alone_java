787fd05d7d13f47ebc0ad430b22aa97e3ee9d662
pts1006 <76603521+pts1006@users.noreply.github.com>
2022-05-21 22:09:04 +0900

    Add files via upload

--- java18/src/com/pts/prc/ClientExample.java
+++ java18/src/com/pts/prc/ClientExample.java
@@ -1,5 +1,7 @@
 package com.pts.prc;
 
+import java.io.InputStream;
+import java.io.OutputStream;
 import java.net.InetSocketAddress;
 import java.net.Socket;
 
@@ -14,6 +16,25 @@ public class ClientExample {
 			System.out.println("[연결 요청]");
 			socket.connect(new InetSocketAddress("localhost", 5001));
 			System.out.println("[연결 성공]");
+			
+			byte[] bytes = null;
+			String message = null;
+			
+			OutputStream os = socket.getOutputStream();
+			message = "Hello Server";
+			bytes = message.getBytes("UTF-8");
+			os.write(bytes);
+			os.flush();
+			System.out.println("[데이터 보내기 성공]");
+			
+			InputStream is = socket.getInputStream();
+			bytes = new byte[100];
+			int readByteCount = is.read(bytes);
+			message = new String(bytes, 0, readByteCount, "UTF-8");
+			System.out.println("[데이터 받기 성공]: " + message);
+			
+			os.close();
+			is.close();
 		} catch (Exception e) {
 			// TODO: handle exception
 		}
